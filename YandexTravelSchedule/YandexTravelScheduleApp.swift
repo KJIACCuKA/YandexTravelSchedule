ea056944e0ad70bb90e3ab9c9bd550ea51c34814
Nikita1380 <160261662+Nikita1380@users.noreply.github.com>
2024-11-09 12:42:29 +0300

    init project

---
+++ YandexTravelSchedule/YandexTravelScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  YandexTravelScheduleApp.swift
+//  YandexTravelSchedule
+//
+//  Created by Никита Козловский on 09.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct YandexTravelScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
