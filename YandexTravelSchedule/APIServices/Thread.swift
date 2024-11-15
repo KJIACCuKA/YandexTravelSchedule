82376551f4fb9a24cf77c3ecdbf80c01d636fa27
KJIACCuKA <160261662+KJIACCuKA@users.noreply.github.com>
2024-11-15 09:27:27 +0300

    generate API

---
+++ YandexTravelSchedule/APIServices/Thread.swift
@@ -0,0 +1,29 @@
+//
+//  Thread.swift
+//  YandexTravelSchedule
+//
+//  Created by Никита Козловский on 15.11.2024.
+//
+
+import Foundation
+import OpenAPIRuntime
+import OpenAPIURLSession
+
+typealias Threads = Components.Schemas.ThreadObject
+
+protocol ThreadServiceProtocol {
+    func getThread(uid: String) async throws -> Threads
+}
+
+final class ThreadService: ThreadServiceProtocol {
+    private let client: Client
+
+    init(client: Client) {
+        self.client = client
+    }
+
+    func getThread(uid: String) async throws -> Threads {
+        let response = try await client.getThread(query: .init(uid: uid))
+        return try response.ok.body.json
+    }
+}
