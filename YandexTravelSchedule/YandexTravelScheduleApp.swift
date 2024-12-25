e3d29d8d9c45a4d81577ffb976b5438a17643358
KJIACCuKA <160261662+KJIACCuKA@users.noreply.github.com>
2024-12-25 09:15:45 +0300

    bug fix

--- YandexTravelSchedule/YandexTravelScheduleApp.swift
+++ YandexTravelSchedule/YandexTravelScheduleApp.swift
@@ -10,16 +10,31 @@ import SwiftUI
 @main
 struct YandexTravelScheduleApp: App {
     
+    
+    @State private var isPresented: Bool = false
     @StateObject var settings = SettingsViewModel(networkService: NetworkService())
     
     var body: some Scene {
         WindowGroup {
-            RootTabView(
-                destinationsViewModel: SearchScreenViewModel(),
-                rootViewModel: RootViewModel(networkService: NetworkService())
-            )
+            if isPresented {
+                RootTabView(
+                    destinationsViewModel: SearchScreenViewModel(),
+                    rootViewModel: RootViewModel(networkService: NetworkService())
+                )
                 .environmentObject(settings)
                 .environment(\.colorScheme, settings.darkMode ? .dark : .light)
+            } else {
+                Image("splashScreen", bundle: nil)
+                    .resizable()
+                    .ignoresSafeArea()
+                    .onAppear {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+                            withAnimation {
+                                self.isPresented = true
+                            }
+                        }
+                    }
+            }
         }
     }
 }
